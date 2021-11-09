23649638301af5b03a1ccf8a502cd4edb5158617
Dan Fellner <dfellner92@gmail.com>
2021-11-09 18:42:29 -0500

    ui is complete on cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import questions from './util/questions';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import questions from "./util/questions";
 
 function App() {
-  const [question, setQuestion] = useState();
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
   let index = String(Math.floor(Math.random() * 2) + 1);
 
   console.log(index);
   useEffect(() => {
-    setQuestion(questions[index]["question"])
-  })
+    setQuestion(questions[index]["question"]);
+    setAnswer(questions[index]["answer"]);
+  }, [setQuestion, setAnswer, questions]);
   console.log(question);
+  console.log(answer);
+
+  const handleShowAnswer = () => {
+    setQuestion(answer);
+  };
+
   return (
     <div className="App">
-      <div>{question}</div>
+      <button onClick={() => handleShowAnswer()}>show answer</button>
+      <div className="card">
+        <div id="text" className="card-text">
+          {question}
+        </div>
+      </div>
     </div>
   );
 }
